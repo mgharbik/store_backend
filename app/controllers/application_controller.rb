11f6c0aefeb9986b6ae53e282152cbff644be4e0
Mohammed Gharbi <simo163@hotmail.co.uk>
2015-06-26 11:49:04 +0200

    changing routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  # since we don't need signup on a backend install
+  def spree_signup_path
+    main_app.login_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  root to: "spree/admin/orders#index"
   mount Spree::Core::Engine, :at => '/'
+
+  devise_for :users do
+    get '/login', :to => "devise/sessions#new"
+    delete '/logout', :to => "devise/sessions#destroy"
+  end
+    
 end
