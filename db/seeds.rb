ef94e4585fcc12f47aa010dc9e43189299de0656
Mohammed Gharbi <simo163@hotmail.co.uk>
2015-06-26 11:50:44 +0200

    adding admin user to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,6 @@
 
 Spree::Core::Engine.load_seed if defined?(Spree::Core)
 Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
+
+user = User.create(email: "spree@example.com", password: "spree123")
+user.spree_roles << Spree::Role.find_or_create_by(name: "admin")
